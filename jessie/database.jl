ae882f8c366a88ded68f7803ac125561374dcaa9
Jessie <hautuoctrongdem@gmail.com>
2016-10-18 00:40:35 -0700

    adapt code to ver 0.4.7,add table and row extraction functions

--- jessie/database.jl
+++ jessie/database.jl
@@ -1,5 +1,6 @@
 # Functions related to SQLite database
 # Create, insert values, and drop (delete) tables once the game exits
+# Please add pkg "SQLite" and "DataArrays" before compiling
 using DataArrays, DataFrames
 using SQLite
 
@@ -7,20 +8,20 @@ db=SQLite.DB("oj_shogi.db")
 
 ## META TABLE
 # create metaTable data table
-function metaTable()
+function set_metaTable()
 	SQLite.query(db,"create table metaTable (key text primary key, value text)")
 	SQLite.query(db,"insert into metaTable values('type',NULL)")
 	SQLite.query(db,"insert into metaTable values('legality',NULL)")
 	SQLite.query(db,"insert into metaTable values('seed',NULL)")
 end
 
-function set_gameType(str::String)
+function set_gameType(str::AbstractString)
 	query = "update metaTable set value = '$(str)' where key = 'type'"
 	println(query)
 	SQLite.query(db,query)
 end
 
-function set_legality(str::String)
+function set_legality(str::AbstractString)
 	query = "update metaTable set value = '$(str)' where key = 'legality'"
 	println(query)
 	SQLite.query(db,query)
@@ -29,7 +30,7 @@ end
 #---------------------------------------------------------------------------------------
 ## MOVES TABLE
 # create movesTable data table
-function movesTable()
+function set_movesTable()
 	SQLite.query(db,"create table movesTable (move_number integer primary key,
 																						move_type text,
 																						sourcex integer,
@@ -38,34 +39,37 @@ function movesTable()
 																						targety integer,
 																						option text,
 																						i_am_cheating integer)")
- println("movesTable set.")
+ println("\nmovesTable set.")
 end
 
 #insert new row
-function set_move(moveNum::Int,moveType::String,
+function set_move(moveNum::Int,moveType::AbstractString,
 									sx::Int,sy::Int,tx::Int,ty::Int,
-									promo::Int,cheat::Int, # put int 1 if the piece is promoted or it's a cheat move
-									droppedPiece::String) # put "" if the move is not a drop move
+									promo::Int,cheat::Int, # insert int 1 if the piece is promoted. same with cheat move
+									droppedPiece::AbstractString) # insert "" if the move is not a drop move
+
 	query_moveNum = "insert into movesTable (move_number) values($(moveNum))"
 	SQLite.query(db,query_moveNum) #update number of moves
 
 	query_moveType = "update movesTable set move_type = '$(moveType)' where move_number = $(moveNum)"
-	SQLite.query(db,query_moveType) # so on so forth
+	SQLite.query(db,query_moveType) #update move_type
 
 	query_sx = "update movesTable set sourcex = $(sx) where move_number = $(moveNum)"
-	SQLite.query(db,query_sx)
+	SQLite.query(db,query_sx) #update source x cord
 
 	query_sy = "update movesTable set sourcey = $(sy) where move_number = $(moveNum)"
-	SQLite.query(db,query_sy)
+	SQLite.query(db,query_sy) #update source y cord
 
 	query_tx = "update movesTable set targetx = $(tx) where move_number = $(moveNum)"
-	SQLite.query(db,query_tx)
+	SQLite.query(db,query_tx) #update target x cord
 
 	query_ty = "update movesTable set targety = $(ty) where move_number = $(moveNum)"
-	SQLite.query(db,query_ty)
+	SQLite.query(db,query_ty) #update target y cord
 
-	# ! -> option  when there's a promotion, piece name -> option when moveType is 'drop'
-	# can't be both
+	# update option column
+	# option <- "!"  when there's a promotion
+	# option <- piece name when moveType is 'drop'
+	# a dropped piece is unpromoted
 	if promo == 1
 		query_opt = "update movesTable set option = '!' where move_number = $(moveNum)"
 	elseif moveType == "drop"
@@ -75,6 +79,7 @@ function set_move(moveNum::Int,moveType::String,
 	end
 	SQLite.query(db,query_opt)
 
+	# update cheat
 	if cheat == 1
 		query_cheat = "update movesTable set i_am_cheating = 'yes' where move_number = $(moveNum)"
 	else
@@ -85,9 +90,33 @@ end
 
 
 #---------------------------------------------------------------------------------------
-## TESTS
+#EXTRACT VALUES FROM TABLES
+
+# return the whole table. table("meta")/table("moves")
+function table(t::AbstractString) # the return type is dataframe, treat it like a multi-dim array
+	if t == "meta"									# https://dataframesjl.readthedocs.io/en/latest/
+		df = SQLite.query(db,"select * from metaTable")
+	else
+		df = SQLite.query(db,"select * from movesTable")
+	end
+	return df
+end
+
+#return details of a row
+function moveDetail(moveNum::Int)
+	df = table("moves")
+	dv = @data([NA,NA,NA,NA,NA,NA,NA,NA])
+	for i in 1:8
+		dv[i] = df[moveNum,i]
+	end
+	return dv
+end
+
+
+#---------------------------------------------------------------------------------------
+##TESTS
 #test movesTable
-movesTable()
+set_movesTable()
 set_move(1,"move",0,0,0,0,0,0,"")
 set_move(2,"drop",0,0,0,0,0,0,"b")
 set_move(3,"move",1,2,3,4,0,1,"")
@@ -95,20 +124,33 @@ set_move(4,"resign",0,0,0,0,1,0,"")
 
 
 #test metaTable
-metaTable()
+set_metaTable()
 set_gameType("standard")
 set_legality("legal")
 
-#---------------------------------------------------------------------------------------
-#Extract tables
-df1= SQLite.query(db,"select * from metaTable")
-df2= SQLite.query(db,"select * from movesTable")
-print(df1) # df is of type data frame, treat it like a multi-dim array
-print(df2)
-# https://dataframesjl.readthedocs.io/en/latest/
-# the value are nullable so use get(df1[1,7]) for example
+#test table extraction
+df1 = table("meta")
+df2 = table("moves") #df stands for dataframe
+println(df1)
+println(df2)
+
+#test row and value extraction
+println("\nDetails of move 3 is:")
+arr = moveDetail(3)
+println(arr)
+println("Type of move 3 array is $(typeof(arr))")
+println("Cheating is $(get(arr[8]))")
+
+# values are nullable => need to use get(df1[1,2])
+println()
+println("Before using get() type is $(typeof(df1[1,2]))") #row1,col2 of df1 is game type
+println("After using get() type is $(typeof(get(df1[1,2])))")
+println("Game type is $(get(df1[1,2]))")
+
 
 
 #delete tables
+#comment out if you want to retain result in .db file
 SQLite.drop!(db,"metaTable")
 SQLite.drop!(db,"movesTable")
+println("\nTables have been deleted.")
