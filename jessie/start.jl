7a46e2e9476dea122fec58f1c0db7886512460ea
Jessie <hautuoctrongdem@gmail.com>
2016-10-21 20:09:08 -0700

    add seed generation in start.jl

--- jessie/start.jl
+++ jessie/start.jl
@@ -1,25 +1,36 @@
+# Orange Julias
 # start.jl <filename> <type(S or M))> <cheating (T for cheating)>
 # to remove .db file go to shell> rm file.db
 include("database.jl")
 
+toGo = 0
+while toGo != 1
+	if length(ARGS) < 2 || (ARGS[2] != "S" && ARGS[2] != "M")
+		println("Input is not valid. Please run the file again! 凸(｀ﾛ´)凸")
+		break
+	else
+		toGo = 1
+		filename = ASCIIString(ARGS[1])
+		gametype = ASCIIString(ARGS[2])
+		#create database
+		init_database(filename)
+		#set game type (standard or minishogi)
+		gametype == "S"? set_gameType(filename,"standard") : set_gameType(filename,"minishogi")
+		#set if cheating allow or not
+		length(ARGS) < 3 || ARGS[3] != "T" ? set_legality(filename,"legal") : set_legality(filename,"cheating")
 
-filename = ASCIIString(ARGS[1])
-gametype = ASCIIString(ARGS[2])
-init_database(filename)
+		#set seed for AI
+		# generate start of unix epoch
+		const UNIXEPOCH = DateTime(1970)
+		# takes current date time and returns number of seconds since unix epoch
+		datetime2unix(dt::DateTime) = (dt - UNIXEPOCH)/1000
+		# generate current unix time for seed
+		seed = Int(datetime2unix(now()))
+		set_seed(filename,seed)
 
-gametype == "S"? set_gameType(filename,"standard") : set_gameType(filename,"minishogi")
-
-length(ARGS) < 3 || ARGS[3] != "T" ? set_legality(filename,"legal") : set_legality(filename,"cheating")
-
-set_move(filename,1,"",0,0,0,0,0,0,"")
-
-table_meta = get_table(filename,"meta")
-table_moves = get_table(filename,"moves")
-
-println(table_meta)
-println(table_moves)
-
-#print args in shell
-# for x in ARGS
-# 	println(x)
-# end
+		table_meta = get_table(filename,"meta")
+		table_moves = get_table(filename,"moves")
+		println(table_meta)
+		println(table_moves)
+	end
+end
