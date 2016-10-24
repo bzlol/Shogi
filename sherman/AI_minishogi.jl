6b178e341b370b47ec77b2d3b8cf06224480fead
Sherman Chow <scc23@sfu.ca>
2016-10-23 20:13:29 -0700

    AI works for both shogi and minishogi

--- sherman/AI_minishogi.jl
+++ sherman/AI_minishogi.jl
@@ -1,3 +1,29 @@
+# AI for for the Japanese chess game Shogi.
+# game logic provided by shogi.jl and move_functions.jl
+
+include("minishogi.jl")
+
+# generate current unix time for seed
+UT = DateTime(1970)
+function datetime2unix()
+    return Int64((DateTime(now())-UT)/1000)
+end
+seed = datetime2unix()
+#println(seed)
+# reseed RNG w/ seed
+srand(seed)
+
+type AI
+    set::Pieces # piece class holds the AI's pieces and cords
+    legal::Array # holds legal moves for the current turn
+    AI(side::Pieces) = new(side,Tuple{Int,Int}[])
+end
+
+function clear_array(A::Array)
+    len = length(A)
+    deleteat!(julia.legal,1:len)
+end
+
 # finds all the possible moves for king given its position
 function king_AI(set::Pieces, legal::Array, piece::ASCIIString)
     # initial x and y cords 
@@ -290,3 +316,227 @@ function pawn_AI(set::Pieces, legal::Array, piece::ASCIIString)
         black_pawn_AI(set,legal,piece) : 
         red_pawn_AI(set,legal,piece)
 end
+
+
+function choose_piece(set::Pieces) 
+    A = collect(keys(AI.set.active))
+    piece = rand(A)
+    return piece
+end
+
+function choose_move(set::Pieces, legal::Array, enemy::Pieces)
+    kills = Dict() 
+    for i = 1:length(legal)
+        piece = enemy.activeS[legal[i]]
+        if haskey(enemy.activeS,legal[i]) == true
+            AI.kill = 1
+            if piece[1] == 'p'
+                get!(kills,1,legal[i])
+            elseif piece[1] == 'n'
+                get!(kills,2,legal[i])
+            elseif piece[1] == 's'
+                get!(kills,1,legal[i])
+            elseif piece[1] == 'l'
+                get!(kills,4,legal[i])
+            elseif piece[1] == 'r' 
+                get!(kills,6,legal[i])
+            elseif piece[1] == 'b' 
+                get!(kills,7,legal[i])
+            elseif piece[1] == 'R'
+                get!(kills,8,legal[i])
+            elseif piece[1] == 'B'
+                get!(kills,5,legal[i])
+            elseif piece[1] == 'k'
+                get!(kills,10,legal[i])
+            else
+                get!(kills,5,legal[i])
+            end
+        end
+    end
+    if length(kills) != 0
+        max = 0
+        for pair in kills
+            pair[1] > max && (max = pair[1])
+        end
+        return Pair(max,kills[max])
+    else
+        cords = rand(legal)
+        return Pair(0,cords)
+    end  
+end
+
+# generates all possible moves, stores into entire legal array
+function generate_moves(set::Pieces, legal::Array, piece)
+    if piece[1] == 'k'
+        legal = king_AI(set,legal,piece)
+    elseif piece[1] == 'r' || piece[1] == 'R'
+        legal = rook_AI(set,legal,piece)
+    elseif piece[1] == 'b' || piece[1] == 'B'
+        legal = bishop_AI(set,legal,piece)
+    elseif piece[1] == 'g'
+        legal = gold_general_AI(set,legal,piece)
+    elseif piece[1] == 's' || piece[1] == 'S'
+        legal = silver_general_AI(set,legal,piece)
+    elseif piece[1] == 'p' || piece[1] == 'P'
+        legal = pawn_AI(set,legal,piece)
+    end
+    return legal
+end
+
+# returns the score
+function score(active::Pieces, inactive::Pieces)
+    # black pieces left on board +1
+    # red pieces left on board -1
+    if active.color == "black"
+        pos = length(active.active)
+        neg = -length(inactive.active)
+        # return the score
+        return pos+neg
+    else    # active.color == "red"
+        pos = length(inactive.active)
+        neg = -length(active.active)
+        # return the score
+        return pos+neg
+    end
+end
+
+# Minimax w/ alpha-beta pruning algorithm for AI
+function minimax_AB(active::Pieces, inactive::Pieces, alpha, beta, depth, limit)
+    # maximizer
+    if active.color == "black"
+        return max_AB(active,inactive,alpha,beta,depth,limit)
+    # minimizer
+    else 
+        return min_AB(active,inactive,alpha,beta,depth,limit)
+    end
+end
+
+function max_AB(active::Pieces, inactive::Pieces, alpha, beta, depth, limit)
+    #println("maximizer turn")
+    best_move::Tuple{Tuple{ASCIIString,Tuple{Int64,Int64}},Int64} 
+    best_move = (("NULL",(0,0)),score(active,inactive))
+    # if depth is reached, recurse back
+    if depth == limit 
+        return best_move
+    else
+        A = collect(keys(active.active)) 
+        #println(A)
+        # generate all possible moves for each piece on black
+        for j = 1:length(A) 
+            piece = A[j]; legal = Tuple{Int,Int}[] 
+            generate_moves(active,legal,piece) 
+            # println(piece)
+            # println(legal)
+            cords = active.active[piece]
+            # analyse each possible move
+            for i = 1:length(legal) 
+                old_alpha::Float64 = alpha
+                old_cords = active.active[piece] # save old coordinates
+                # simulate move
+                update_piece(active,piece,legal[i])
+                dead::ASCIIString = check_kill(inactive,legal[i])
+                #println("dead = $dead")
+                # recursive call to minimizer
+                AB = min_AB(inactive,active,alpha,beta,depth+1,limit)
+                alpha = max(alpha,AB[2])
+                # update best_move
+                old_alpha < alpha && (best_move = ((piece,legal[i]),alpha))  
+                # reverse simulation
+                update_piece(active,piece,old_cords)
+                dead != "NULL" && raise_dead(inactive,dead,legal[i])
+                # check for alpha-beta pruning
+                if alpha >= beta 
+                    #println("pruned")
+                    return best_move
+                end     
+            end
+        end
+    end
+    return best_move
+end
+
+function min_AB(active::Pieces, inactive::Pieces, alpha, beta, depth, limit)
+    #println("minimizer turn")
+    best_move::Tuple{Tuple{ASCIIString,Tuple{Int64,Int64}},Int64} 
+    best_move = (("NULL",(0,0)),score(active,inactive))
+    # if depth is reached, recurse back
+    if depth == limit 
+        return best_move
+    else
+        # generate all possible moves for each piece on black
+        A = collect(keys(active.active)) 
+        for j = 1:length(A) 
+            piece = A[j]; legal = Tuple{Int,Int}[] 
+            generate_moves(active,legal,piece) 
+            # println(piece)
+            # println(legal)
+            # analyse each possible move
+            for i = 1:length(legal)
+                old_beta::Float64 = beta
+                old_cords = active.active[piece] # save old coordinates
+                # simulate move
+                update_piece(active,piece,legal[i])
+                dead::ASCIIString = check_kill(inactive,legal[i])
+                #println("$piece killed $dead")
+                # recursive call to minimizer
+                AB = max_AB(inactive,active,alpha,beta,depth+1,limit)
+                beta = min(beta,AB[2])
+                # update best_move
+                old_beta > beta && (best_move = ((piece,legal[i]),beta))
+                # reverse simulation
+                update_piece(active,piece,old_cords)
+                dead != "NULL" && raise_dead(inactive,dead,legal[i])
+                # check for alpha-beta pruning
+                if alpha >= beta
+                    #println("pruned")
+                    return best_move
+                end  
+            end
+        end
+    end
+    return best_move
+ end
+
+### TESTING
+
+# initialize sides and AI - with its side
+red = Pieces("red")
+fill_red(red)
+black = Pieces("black")
+fill_black(black)
+
+# legal = Tuple{Int,Int}[] 
+# legal = generate_moves(black,legal,"s2")
+# println(legal)
+#julia = AI(black)
+
+# update_piece(red,"p1",(1,6))
+# update_piece(red,"p2",(2,6))
+# update_piece(red,"p3",(3,6))
+# update_piece(red,"p4",(4,6))
+# update_piece(red,"p5",(5,6))
+# update_piece(red,"p6",(6,6))
+# update_piece(red,"p7",(7,6))
+# update_piece(red,"p8",(8,6))
+# update_piece(red,"p9",(9,6))
+
+minimax_AB(black,red,-Inf,Inf,0,3)
+# for Pair in black.active
+#     println(Pair)
+# end
+
+# cords = black.active["n1"]
+# println(black.active["n1"])
+# println(black.activeS[cords])
+
+# fill the AI pieces 
+# fill_black(julia.set)
+
+
+
+
+
+
+
+
+
--- sherman/play.jl
+++ sherman/play.jl
@@ -73,7 +73,7 @@ if game_type == "S" && opponent == "AI"
 			move = minimax_AB(julia.set,red,-Inf,Inf,0,3)
 			piece = move[1][1]; cords = move[1][2];
 			println(move[2])
-			move_piece(GB,julia.set,red,piece,cords)
+			AI_move_piece(GB,julia.set,red,piece,cords)
 			if GB.status == 0
 				turn ==  0 ? 
 					println("Red king slain, Black wins") :
@@ -175,6 +175,155 @@ if game_type == "S" && opponent == "AI"
 		end
 	end
 	println("Game Over")
+
+elseif game_type == "M" && opponent == "AI"
+	include("minishogi.jl")
+	include("move_functions_minishogi.jl")
+	include("AI_minishogi.jl")
+	println("\nLet's play Shogi\n")
+
+	# create instance of game board
+	GB = Board() 
+	# create red pieces for user
+	red = Pieces("red") 
+	# set starting coordinates of red pieces
+	fill_red(red)
+	# create black pieces 
+	black = Pieces("black") 
+	# set starting coordinates of black pieces
+	fill_black(black) 
+	# set AI to black
+	julia = AI(black)
+	# set red and black pieces onto the board
+	init_board(GB,red,julia.set)
+
+
+	while(GB.status != 0) # while game is not over
+		@label start_minishogi_AI
+		display_board(GB,red,julia.set) # display current state of board
+		if GB.turn % 2 == 0 # decide whos turn
+			turn = 0 # black turn
+			println("Black Turn:") 
+		else
+			turn = 1 # red turn
+			# user instructions
+			println("User Instructions:")
+			println("Enter 'move' 'piece' 'x coordinate' 'y coordinate'")
+			println("Enter 'cheat' 'piece' 'x coordinate' 'y coordinate'")
+			println("Enter 'quit' to resign from the game\n")
+
+			println("Red Turn:")
+		end
+		GB.turn += 1
+		# AI turn
+		if turn == 0
+			move = minimax_AB(julia.set,red,-Inf,Inf,0,3)
+			piece = move[1][1]; cords = move[1][2];
+			println(move[2])
+			AI_move_piece(GB,julia.set,red,piece,cords)
+			if GB.status == 0
+				turn ==  0 ? 
+					println("Red king slain, Black wins") :
+					println("Black king slain, Red wins.") 
+				display_board(GB,red,julia.set)		
+	 		end 
+	 	# User turn
+	 	else 
+			# obtain instruction from user
+			user_input = input()
+			#length(user_input) == 0 && continue # skip turn
+
+			# check for invalid user input
+			if length(user_input) < 6 && user_input!="quit"
+				println("\nInvalid Input\n")
+				GB.turn -= 1
+				@goto start_minishogi_AI
+			elseif user_input[1:4]!="move" && user_input[1:4]!="quit" && contains(user_input,"cheat")==false
+				println("Invalid Input\n")
+				GB.turn -= 1
+				@goto start_minishogi_AI
+			end
+
+			user_input = chomp(user_input) # remove newline char
+			len = length(user_input)
+
+			cords::Tuple{Int,Int}
+			# extract coordinates 
+			x = parse(Int,user_input[len-2])
+			y = parse(Int,user_input[len])
+			cords = (x,y)
+			# extract move
+			move = user_input[1:4] 
+
+			# for testing
+			if contains(user_input,"cheat")
+				piece = strip(user_input[6:len-3])
+				turn == 0 ? move_piece(GB,julia.set,red,piece,cords) :
+				move_piece(GB,red,julia.set,piece,cords)
+				# king kill
+				if GB.status == 0
+					turn ==  0 ? 
+						println("Red king slain, black wins") :
+						println("black king slain, Red wins.") 
+					display_board(GB,red,julia.set)		
+		 		end 
+			# parse instruction
+			elseif move != "quit"
+				# extract piece and determine which move function to call
+				piece = strip(user_input[5:len-3]); t = piece[1] # type of piece
+				if t == 'p' || t == 'P'
+					turn == 0 ?
+						move_julia.set_p(GB,julia.set,red,piece,cords) : 
+						move_red_p(GB,red,julia.set,piece,cords)
+				elseif t == 'k'
+					turn == 0 ?
+						move_king(GB,julia.set,red,piece,cords) : 
+						move_king(GB,red,julia.set,piece,cords)
+				elseif t == 'g'
+					turn == 0 ?
+						move_julia.set_g(GB,julia.set,red,piece,cords) : 
+						move_red_g(GB,red,julia.set,piece,cords)
+				elseif t == 's' || t == 'S'
+					turn == 0 ? 
+						move_julia.set_s(GB,julia.set,red,piece,cords) :
+						move_red_s(GB,red,julia.set,piece,cords)
+				elseif t == 'n'|| t == 'N'
+					turn == 0 ?
+						move_julia.set_n(GB,julia.set,red,piece,cords) : 
+						move_red_n(GB,red,julia.set,piece,cords)
+				elseif t == 'b' || t == 'B'
+					turn == 0 ?
+						move_bishop(GB,julia.set,red,piece,cords) :
+						move_bishop(GB,red,julia.set,piece,cords)
+				elseif t == 'r' || t == 'R'
+					turn == 0 ?
+						move_rook(GB,julia.set,red,piece,cords) :
+						move_rook(GB,red,julia.set,piece,cords)
+				elseif t == 'l' || t == 'L'
+					turn == 0 ?
+						move_lancerB(GB,julia.set,red,piece,cords) :
+						move_lancerR(GB,red,julia.set,piece,cords)
+				else
+					println("Invalid Input")
+				end
+
+				if GB.status == 0
+					turn ==  0 ? 
+						println("Red king slain, Black wins") :
+						println("Black king slain, Red wins.") 
+					display_board(GB,red,black)		
+		 		end 
+			else 
+				GB.status = 0 # user resigns
+				turn == 0 ? 
+					println("Black concedes, Red wins.") :
+					println("Red concedes, Black wins.")
+			end
+		end
+	end
+	println("Game Over")
+
+
 
 elseif game_type == "S"
 	include("shogi.jl")
@@ -324,7 +473,7 @@ elseif game_type == "S"
 
 elseif game_type == "M"
 	include("minishogi.jl")
-	include("mov_functions_minishogi.jl")
+	include("move_functions_minishogi.jl")
 	println("\nLet's play Minishogi\n")
 	### INITIALIZE GAME 
 
