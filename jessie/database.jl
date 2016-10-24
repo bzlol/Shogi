4a67920dcaf4d8e231021369b5c58b28579194aa
Jessie <hautuoctrongdem@gmail.com>
2016-10-24 14:46:01 -0700

    fix extension issues

--- jessie/database.jl
+++ jessie/database.jl
@@ -6,8 +6,8 @@ using DataArrays, DataFrames
 using SQLite
 
 #init database. for start.jl only
-function init_database(f::AbstractString)
-	db=SQLite.DB("$(f).db")
+function init_database(f::ASCIIString)
+	db=SQLite.DB("$(f)")
 
 # create meta data table
 	SQLite.query(db,"create table meta (key text primary key, value text)")
@@ -18,13 +18,13 @@ function init_database(f::AbstractString)
 
 # create moves data table
 	SQLite.query(db,"create table moves (move_number integer primary key,
-																							move_type text,
-																							sourcex integer,
-																							sourcey integer,
-																							targetx integer,
-																							targety integer,
-																							option text,
-																							i_am_cheating integer)")
+																			move_type text,
+																			sourcex integer,
+																			sourcey integer,
+																			targetx integer,
+																			targety integer,
+																			option text,
+																			i_am_cheating integer)")
 	println("\nmoves table set.\n")
 end
 
@@ -32,21 +32,21 @@ end
 ## SET META TABLE
 
 function set_gameType(f::ASCIIString,gt::ASCIIString)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "update meta set value = '$(gt)' where key = 'type'"
 	#println(query)
 	SQLite.query(db,query)
 end
 
 function set_legality(f::ASCIIString,l::ASCIIString)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "update meta set value = '$(l)' where key = 'legality'"
 	#println(query)
 	SQLite.query(db,query)
 end
 
 function set_seed(f::ASCIIString,s::Int)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "update meta set value = '$(s)' where key = 'seed'"
 	df = SQLite.query(db,query)
 end
@@ -60,7 +60,7 @@ function set_move(f::ASCIIString,
 									sx::Int,sy::Int,tx::Int,ty::Int,
 									promo::Int,cheat::Int, # insert int 1 if the piece is promoted. same with cheat move
 									droppedPiece::ASCIIString) # insert "" if the move is not a drop move
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	mNum = get_totalMoves(f) + 1
 	query_mNum = "insert into moves (move_number) values($(mNum))"
 	SQLite.query(db,query_mNum) #update number of moves
@@ -108,7 +108,7 @@ end
 
 #input:filename, table name. return the whole table, type dataframe
 function get_table(f::ASCIIString,t::AbstractString) # the return type is DataFrame, treat it like a multi-dim array
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	if t == "meta"									# https://dataframesjl.readthedocs.io/en/latest/
 		df = SQLite.query(db,"select * from meta")
 	else
@@ -121,7 +121,7 @@ end
 ## GET VALUES FROM META TABLE
 #input: filename. return string game type
 function get_gameType(f::ASCIIString)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT value FROM meta where key = 'type'"
 	df = SQLite.query(db,query)
 	isnull(df[1,1]) == true ? (return "empty") : (return get(df[1,1]))
@@ -129,7 +129,7 @@ end
 
 
 function ischeatGame(f::ASCIIString)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT value FROM meta where key = 'legality'"
 	df = SQLite.query(db,query)
 	isnull(df[1,1]) == true ? (return "empty") : (l = get(df[1,1]))
@@ -138,14 +138,14 @@ end
 
 #input: filename. return string legality
 function get_legality(f::ASCIIString)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT value FROM meta where key = 'legality'"
 	df = SQLite.query(db,query)
 	isnull(df[1,1]) == true ? (return "empty") : (return get(df[1,1]))
 end
 #input: filename. return int seed
 function get_seed(f::ASCIIString)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT value FROM meta where key = 'seed'"
 	df = SQLite.query(db,query)
 	isnull(df[1,1]) == true ? (return 0) : (return parse(get(df[1,1])))
@@ -165,14 +165,14 @@ function get_row(f::ASCIIString,mNum::Int)
 end
 #input: filename. return an int total rows/moves in the moves
 function get_totalMoves(f::ASCIIString)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT Count(*) FROM moves"
 	df = SQLite.query(db, query)
 	return get(df[1,1])
 end
 #input:filename, move number. return a string move type at a specific move_num
 function get_moveType(f::ASCIIString,mNum::Int)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT move_type FROM moves where move_number = $(mNum)"
 	df = SQLite.query(db, query)
 	isnull(df[1,1]) == true? (return "NULL") : (return get(df[1,1]) )
@@ -180,7 +180,7 @@ end
 
 #input:filename, move number. return a tuple source coord at a specific move_num
 function get_sourceCords(f::ASCIIString,mNum::Int)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query_getSourcex = "SELECT sourcex FROM moves where move_number = $(mNum)"
 	query_getSourcey = "SELECT sourcey FROM moves where move_number = $(mNum)"
 	dfx = SQLite.query(db, query_getSourcex)
@@ -197,7 +197,7 @@ end
 
 #input:filename, move number. return a tuple target coord at a specific move_num
 function get_targetCords(f::ASCIIString,mNum::Int)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query_getTargetx = "SELECT targetx FROM moves where move_number = $(mNum)"
 	query_getTargety = "SELECT targety FROM moves where move_number = $(mNum)"
 	dfx = SQLite.query(db, query_getTargetx)
@@ -214,7 +214,7 @@ function get_targetCords(f::ASCIIString,mNum::Int)
 end
 #input:filename, move number. return bool if cheating at a specific move_num
 function ischeatMove(f::ASCIIString,mNum::Int)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT i_am_cheating FROM moves where move_number = $(mNum)"
 	df = SQLite.query(db, query)
 	isnull(df[1,1]) == true ? (return false) : (return true)
@@ -222,7 +222,7 @@ end
 
 #input:filename, move number. return bool if a piece is promoted at a specific move_num
 function ispromoted(f::ASCIIString,mNum::Int)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT option FROM moves where move_number = $(mNum)"
 	df = SQLite.query(db, query)
 	isnull(df[1,1]) == true || get(df[1,1]) != "!"? (return false) : (return true)
@@ -230,14 +230,14 @@ end
 
 #input:filename, move number. return bool if a piece is dropped at a specific move_num
 function isdropped(f::ASCIIString,mNum::Int)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT option FROM moves where move_number = $(mNum)"
 	df = SQLite.query(db, query)
 	isnull(df[1,1]) == true || get(df[1,1]) == "!" ? (return false) : (return true)
 end
 #input:filename, move number. return string piece name of the dropped piece at a specific move_num
 function get_droppedPiece(f::ASCIIString,mNum::Int)
-	db = SQLite.DB("$(f).db")
+	db = SQLite.DB("$(f)")
 	query = "SELECT option FROM moves where move_number = $(mNum)"
 	df = SQLite.query(db, query)
 	isdropped(f,mNum) == true? ( return get( df[1,1] ) ) : (return "")
